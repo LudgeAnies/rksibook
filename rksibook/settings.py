3d411cc646c86c0a2b33c94aa529dd9d48c4c05d
Egor Liadsky <47715067+Egor-Liadsky@users.noreply.github.com>
2021-11-07 20:24:10 +0300

    adding postgresql to the project

--- rksibook/settings.py
+++ rksibook/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'rksibook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rksi_book',
+        'USER': 'postgres',
+        'HOST': '127.0.0.1',
+        'PASSWORD': 'root',
+        'PORT': '5432'
     }
 }
 
